2d7035872679dfd5942fd62b0029b074f3d265c9
Sewoong <lympsw12@naver.com>
2023-03-15 02:58:40 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,64 @@
+package jpabook.jpashop.api;
+
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * xToOne 관계 최적화
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+    private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAll(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //LAZY가 강제 초기화
+            order.getDelivery().getAddress(); //LAZY가 강제 초기화
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV2(){
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+        List<OrderSimpleQueryDto> result = orders.stream()
+                .map(o -> new OrderSimpleQueryDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV3(){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<OrderSimpleQueryDto> result = orders.stream()
+                .map(o -> new OrderSimpleQueryDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4(){
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
+
+
+
+
+}
---
+++ src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class ItemServiceTest {
+
+    @Autowired ItemService itemService;
+    @Autowired ItemRepository itemRepository;
+
+    @Test
+    public void 아이템등록() throws Exception{
+        Book book = new Book();
+        book.setName("aaa");
+        Item item = book;
+
+        itemService.saveItem(item);
+
+        assertEquals(item, itemRepository.findOne(item.getId()));
+    }
+}
